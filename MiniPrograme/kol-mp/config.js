20a177a1b1391810af746b663a69a8fef7a3d02e
haizhou <dinghaizhou@optaim.com>
2019-08-22 15:27:39 +0800

    修改图片mode值

--- MiniPrograme/kol-mp/config.js
+++ MiniPrograme/kol-mp/config.js
@@ -4,8 +4,8 @@ const config = {
     // API_BASE_URL: 'http://kol.com/miniprogram'
 }
 
-// wx90892d0a10541e6b
-// wx3604ec8ada3c5e7c
+// wx90892d0a10541e6b 
+// wx3604ec8ada3c5e7c 测试 id
 export default config
 
 
