498358faaea6d6476d768500d5bf15643c3c94d5
haizhou <dinghaizhou@optaim.com>
2019-10-17 18:24:46 +0800

    koa2-project

---
+++ Node/koa2-project/config.js
@@ -0,0 +1,12 @@
+const config = {
+  port: 3001,
+  database: {
+    DATABASE: 'koa_demo',
+    USERNAME: 'root',
+    PASSWORD: 'root',
+    PORT: 8889,
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
