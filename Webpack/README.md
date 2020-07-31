65cc6e53a8dda9f394043037aa8f0d385cdf1104
haizhou <291727105@qq.com>
2020-07-31 15:22:47 +0800

    webpack demo

---
+++ Webpack/README.md
@@ -0,0 +1,4 @@
+# 局部安装
+npm install webpack webpack-cli --save-dev
+# 打包命令
+1. webpack（局部安装 ./node_modules/.bin/webpack） --mode production ./app.js -o ./bundle.js
---
+++ Webpack/demo_01/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+document.querySelector('h1').style.color = 'red'
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
